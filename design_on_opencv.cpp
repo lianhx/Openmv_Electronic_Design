41ef5b630e2a6d9832acea138dc9dd4690e94c30
ssmem <31817404+ssmem@users.noreply.github.com>
2019-03-27 13:25:01 +0800

    Update design_on_opencv.cpp

--- design_on_opencv.cpp
+++ design_on_opencv.cpp
@@ -3,56 +3,88 @@ using namespace std;
 #include<opencv2/opencv.hpp>
 using namespace cv;
 
-void find_rect16(Mat &src, int limit_value)
+void find_rect( Mat &src, int limit_value)
 {
-	Mat example(Size(src.cols, src.rows), CV_8UC3);
-	for (int i = 0; i < src.rows - 2; i+=4)
-	{
-		for (int j = 0; j < src.cols - 2; j+=4)
-		{
-			if (((src.at<Vec3b>(i, j)[0] + src.at<Vec3b>(i + 1, j)[0] + src.at<Vec3b>(i + 2, j)[0] + src.at<Vec3b>(i + 3, j)[0] +
-				src.at<Vec3b>(i, j + 1)[0] + src.at<Vec3b>(i + 1, j + 1)[0] + src.at<Vec3b>(i + 2, j + 1)[0] + src.at<Vec3b>(i + 3, j + 1)[0] +
-				src.at<Vec3b>(i, j + 1)[0] + src.at<Vec3b>(i + 1, j + 1)[0] + src.at<Vec3b>(i + 2, j + 1)[0] + src.at<Vec3b>(i + 3, j + 1)[0] +
-				src.at<Vec3b>(i, j + 1)[0] + src.at<Vec3b>(i + 1, j + 1)[0] + src.at<Vec3b>(i + 2, j + 1)[0] + src.at<Vec3b>(i + 3, j + 1)[0]) / 16) < limit_value)
-			circle(example, Point(j, i), 1, Scalar(0, 255, 0), 2);
-		}
-	}
-	imshow("example", example);
-}
-void find_rect9(Mat &src, int limit_value)
-{
-	Mat example(Size(src.cols, src.rows), CV_8UC3);
-	for (int i = 0; i < src.rows - 2; i += 4)
-	{
-		for (int j = 0; j < src.cols - 2; j += 4)
-		{
-			if (((src.at<Vec3b>(i, j)[0] + src.at<Vec3b>(i + 1, j)[0] + src.at<Vec3b>(i + 2, j)[0] +
-				src.at<Vec3b>(i, j + 1)[0] + src.at<Vec3b>(i + 1, j + 1)[0] + src.at<Vec3b>(i + 2, j + 1)[0] +
-				src.at<Vec3b>(i, j + 1)[0] + src.at<Vec3b>(i + 1, j + 1)[0] + src.at<Vec3b>(i + 2, j + 1)[0] + 
-				src.at<Vec3b>(i, j + 1)[0] + src.at<Vec3b>(i + 1, j + 1)[0] + src.at<Vec3b>(i + 2, j + 1)[0] ) / 9) > limit_value)
-				circle(example, Point(j, i), 1, Scalar(0, 255, 0), 2);
-		}
-	}
-	imshow("example", example);
-}
+	threshold(src, src, limit_value, 255, THRESH_BINARY);
 
+	int _width = src.cols;
+	int _height = src.rows;
+	int half_width = 0.5 * _width;
+	int half_height = 0.5 * _height;
+
+	int left_x = 0, left_y = 0;
+	int right_x = 0, right_y = 0;
+	int up_x = 0, up_y = 0;
+	int down_x = half_width, down_y = _height;
+
+	int left_flag = 0;
+	int right_flag = 0;
+	int up_flag = 0;
+	int down_flag = 0;
+	for (int i = half_width, j = half_height; i > 5; i -= 2)
+	{
+		int box_mean = (src.at<cv::Vec3b>(j - 1, i - 1)[0] + src.at<cv::Vec3b>(j - 1, i)[0] + src.at<cv::Vec3b>(j - 1, i + 1)[0] +
+			src.at<cv::Vec3b>(j, i - 1)[0] + src.at<cv::Vec3b>(j, i)[0] + src.at<cv::Vec3b>(j, i + 1)[0] +
+			src.at<cv::Vec3b>(j + 1, i - 1)[0] + src.at<cv::Vec3b>(j + 1, i)[0] + src.at<cv::Vec3b>(j + 1, i + 1)[0]) / 9;
+		if ((box_mean > limit_value)&&(!left_flag))
+		{
+			printf("[%d,%d]", i, j); left_x = i; left_y = j;
+			left_flag = 1;
+		}		
+	}
+
+	for (int i = half_width, j = half_height; i < _width - 5; i += 2)
+	{
+		int box_mean = (src.at<cv::Vec3b>(j - 1, i - 1)[0] + src.at<cv::Vec3b>(j - 1, i)[0] + src.at<cv::Vec3b>(j - 1, i + 1)[0] +
+			src.at<cv::Vec3b>(j, i - 1)[0] + src.at<cv::Vec3b>(j, i)[0] + src.at<cv::Vec3b>(j, i + 1)[0] +
+			src.at<cv::Vec3b>(j + 1, i - 1)[0] + src.at<cv::Vec3b>(j + 1, i)[0] + src.at<cv::Vec3b>(j + 1, i + 1)[0]) / 9;
+		if ((box_mean > limit_value)&&(!right_flag))
+		{
+			printf("[%d,%d]", i, j); right_x = i; right_y = j;
+			right_flag = 1;
+		}
+			
+	}
+
+	for (int i = half_width, j = half_height; j > 5; j -= 2)
+	{
+		int box_mean = (src.at<cv::Vec3b>(j - 1, i - 1)[0] + src.at<cv::Vec3b>(j - 1, i)[0] + src.at<cv::Vec3b>(j - 1, i + 1)[0] +
+			src.at<cv::Vec3b>(j, i - 1)[0] + src.at<cv::Vec3b>(j, i)[0] + src.at<cv::Vec3b>(j, i + 1)[0] +
+			src.at<cv::Vec3b>(j + 1, i - 1)[0] + src.at<cv::Vec3b>(j + 1, i)[0] + src.at<cv::Vec3b>(j + 1, i + 1)[0]) / 9;
+		if ((box_mean > limit_value) && (!up_flag))
+		{
+			printf("[%d,%d]", i, j); up_x = i; up_y = j;
+			up_flag = 1;
+		}		
+	}
+
+	for (int i = half_width, j = half_height; j < _height - 5; j += 2)
+	{
+		int box_mean = (src.at<cv::Vec3b>(j-1, i-1)[0] + src.at<cv::Vec3b>(j - 1, i)[0] + src.at<cv::Vec3b>(j - 1, i + 1)[0] +
+			src.at<cv::Vec3b>(j, i-1)[0] + src.at<cv::Vec3b>(j, i)[0] + src.at<cv::Vec3b>(j, i + 1)[0] +
+			src.at<cv::Vec3b>(j + 1, i - 1)[0] + src.at<cv::Vec3b>(j + 1, i)[0] + src.at<cv::Vec3b>(j + 1, i + 1)[0])/9;
+		if ((box_mean > limit_value)&&(!down_flag))
+		{
+			printf("[%d,%d]", i, j); down_x = i; down_y = j;
+			down_flag = 1;
+		}
+	}
+
+	rectangle(src, Rect(left_x, up_y, right_x - left_x, down_y - up_y), Scalar(0, 255, 0), 3);
+}
 
 
 
 
 int main()
 {
-
 	Mat src = imread("demo.jpg");
 
+	find_rect(src, 100);
 
-	threshold(src, src, 120,255,0);
-	find_rect9(src, 200);
-	//findContours(src, contours, 0, 0);
 	imshow("0",src);
 	waitKey(0);
 
-	//system("pause");
 	return 0;
 }
 
