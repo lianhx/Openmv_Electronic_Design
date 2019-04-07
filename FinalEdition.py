bcea70cec75fb2a66673f3fff0f5d94dfd4926c4
ssmem <31817404+ssmem@users.noreply.github.com>
2019-04-07 14:48:43 +0800

    Update FinalEdition.py

--- FinalEdition.py
+++ FinalEdition.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Apr  6 23:15:20 2019
 
-@author: acer
-"""
 #import sensor, image, time
 #from pyb import UART
 
@@ -14,13 +10,12 @@ Created on Sat Apr  6 23:15:20 2019
 #sensor.set_auto_gain(False) # must be turned off for color tracking
 #sensor.set_auto_whitebal(False) # must be turned off for color tracking
 #clock = time.clock()
-from struct import *
-
+from struct import pack
 
 thresholds = [(210, 255)] # grayscale thresholds设置阈值
-buf_x=[256,257,32]
-buf_y=[0,0,0]
-frame_time=0.12
+buf_x=[256,121,32]
+buf_y=[560,450,235]
+frame_time=0.10
 
 def move_():
     buf_x[2]=buf_x[1]
@@ -50,17 +45,7 @@ speed_last_y=(buf_y[1]-buf_y[2])/frame_time
 a_speed_x=(speed_now_x-speed_last_x)/frame_time
 a_speed_y=(speed_now_y-speed_last_y)/frame_time
 
-print("x 速度：",speed_now_x)
-print("x 加速度：",a_speed_x)
-#output_str="[%d,%d]" % (c.x(),c.y())
-
-head_str=bytearray([0x35,0x46,0x57,0x24])
-print(pack('hh',buf_x[0],buf_y[0]))
-print(pack('hh',int(speed_now_x),int(speed_now_y)))
-print(pack('hh',int(a_speed_x),int(a_speed_y)))
-
-
-print('you send:',head_str)
-#uart.write(output_str+'\r\n')
+datap=pack('bbbbhhhhhh',0x35,0x46,0x57,0x24,buf_x[0],buf_y[0],int(speed_now_x),int(speed_now_y),int(a_speed_x),int(a_speed_y))
+print('you send:',datap)
+#uart.write(datap+'\r\n')
 #print("FPS %f" % clock.fps())
-
