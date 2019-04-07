2b957dddafdead2f9310a171a09bf29a9ab05642
ssmem <31817404+ssmem@users.noreply.github.com>
2019-04-07 16:52:50 +0800

    Update design_on_opencv.py

--- design_on_opencv.py
+++ design_on_opencv.py
@@ -1,46 +1,33 @@
 # -*- coding: utf-8 -*-
 import cv2 as cv
-def find_rect(src,gray):
-    up_flag = 1
-    down_flag = 1
-    left_flag = 1
-    right_flag = 1
+
+def find_rect_and_ball(src,gray):
+    u_flag = 1;d_flag = 1;l_flag = 1;r_flag = 1
     rect=[0,0,0,0]
     ret,thresh=cv.threshold(gray,100,255,cv.THRESH_BINARY)
-    height = gray.shape[0]
-    width = gray.shape[1]
+    height = gray.shape[0];width = gray.shape[1]
     
     for row in range(int(0.5*height),0,-1):
         col = int(0.5*width)
         pv = thresh[row, col]
-        if((pv>150)&(up_flag)):
-            up_flag=0
-            rect[1]=row
-            cv.circle(src,(col,row),3,(0,0,213),-1)
+        if((pv>150)&(u_flag)):u_flag=0;rect[1]=row;cv.circle(src,(col,row),3,(0,0,213),-1)
             
     for row in range(int(0.5*height),height):
         col = int(0.5*width)
         pv = thresh[row, col]
-        if((pv>150)&(down_flag)):
-            down_flag=0
-            rect[3]=row
-            cv.circle(src,(col,row),3,(0,0,213),-1)
+        if((pv>150)&(d_flag)):d_flag=0;rect[3]=row;cv.circle(src,(col,row),3,(0,0,213),-1)
         
     for col in range(int(0.5*width),0,-1):
         row = int(0.5*height)
         pv = thresh[row, col]
-        if((pv>150)&(left_flag)):
-            left_flag=0
-            rect[0]=col
-            cv.circle(src,(col,row),3,(0,0,213),-1)
+        if((pv>150)&(l_flag)):l_flag=0;rect[0]=col;cv.circle(src,(col,row),3,(0,0,213),-1)
             
     for col in range(int(0.5*width),width):
         row = int(0.5*height)
         pv = thresh[row, col]
-        if((pv>150)&(right_flag)):
-            right_flag=0
-            rect[2]=col
-            cv.circle(src,(col,row),3,(0,0,213),-1)
+        if((pv>150)&(r_flag)):r_flag=0;rect[2]=col;cv.circle(src,(col,row),3,(0,0,213),-1)
+    locate =[0,0]
+    count = 0
     for col in range(rect[0]+5,rect[2]-5,2):
         for row in range(rect[1]+5,rect[3]-5,2):
             pv1 = [thresh[row-1, col-1],thresh[row-1, col],thresh[row-1, col+1]]
@@ -50,16 +37,20 @@ def find_rect(src,gray):
             aver += (int(pv2[0])+int(pv2[1])+int(pv2[2]))/9
             aver += (int(pv3[0])+int(pv3[1])+int(pv3[2]))/9
             if(aver > 240):
-                cv.circle(src,(col,row),3,(0,0,213),-1)
-        cv.rectangle(src, (rect[0],rect[1]), (rect[2],rect[3]), (255,0,0),3)
-    cv.imshow("bin", thresh)
-    cv.imshow("src", src)
-    
-    
+                locate[0] += col
+                locate[1] += row
+                count = count + 1
+    if(count > 0):
+        locate[0]=int(locate[0]/count)
+        locate[1]=int(locate[1]/count)
+    cv.rectangle(src, (rect[0],rect[1]), (rect[2],rect[3]), (255,0,0),3)
+    return locate
+
 image = cv.imread("demo.jpg")
 GrayImage=cv.cvtColor(image,cv.COLOR_BGR2GRAY)
-find_rect(image,GrayImage)
 
-
+located = find_rect_and_ball(image,GrayImage)
+cv.circle(image,(located[0],located[1]),3,(0,0,233),-1)
+cv.imshow("image", image)
 cv.waitKey(0)
 cv.destroyAllWindows()
